879fd70db102c8821fe590f2ee370ad95b92cb2b
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-08-24 22:04:12 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song
+
+    attr_accessor :name , :artist , :genre 
+
+    @@count=0
+    @@genres=[]
+    @@artists=[]
+
+    def initialize (name,artist,genre)
+        @name=name
+        @artist=artist
+        @genre=genre 
+        @@count +=1
+        @@genres << genre 
+        @@artists << artist 
+
+    end 
+
+    def self.count
+        @@count 
+    end 
+
+    def self.genres
+        @@genres.uniq
+    end 
+
+    def self.artists
+        @@artists.uniq
+    end 
+
+    def self.genre_count
+        result={}
+        @@genres.each do |gen|
+            result[gen]= (result[gen] || 0) + 1
+        end 
+        result
+    end
+
+    def self.artist_count
+        result={}
+        @@artists.each do |art|
+            result[art]= (result[art] || 0) + 1
+        end 
+        result
+    end
+
+
+
+end 
